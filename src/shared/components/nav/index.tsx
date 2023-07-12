b06f93a56a35d12514200c0a3e1bb1fdd32a684c
Błażej Plichta <b.plichta96@gmail.com>
2023-07-12 19:53:25 +0200

    fix nav responsiveness

--- src/shared/components/nav/index.tsx
+++ src/shared/components/nav/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { BottomNavigation, BottomNavigationAction } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 import navList from './nav';
 
@@ -11,51 +11,44 @@ const navElements = () =>
 			label={element.name}
 			icon={element.icon}
 			value={element.name}
-			to={`/DriversWorkPlan/${element.name}`}
+			to={
+				element.name === 'Dashboard'
+					? '/DriversWorkPlan/'
+					: `/DriversWorkPlan/${element.name}`
+			}
 			component={Link}
 			sx={{ maxHeight: '100px' }}
 		/>
 	));
 
 export default function MainNavList() {
-	const [value, setValue] = React.useState('Dashboard');
+	const { pathname } = useLocation();
+	const [value, setValue] = React.useState(pathname.split('/')[2]);
 	const handleChange = (event: React.SyntheticEvent, newValue: string) => {
 		setValue(newValue);
 	};
 
-	const BottomNav = () => (
+	return (
 		<BottomNavigation
-			sx={{ position: 'fixed', bottom: 0, right: 0, left: 0 }}
-			value={value}
-			onChange={handleChange}
-		>
-			{navElements()}
-		</BottomNavigation>
-	);
-
-	const LeftNav = () => (
-		<BottomNavigation
+			showLabels
 			sx={{
-				flexDirection: 'column',
 				position: 'fixed',
-				top: 0,
+				flexDirection: ['row', 'column'],
+				top: [null, 0],
 				bottom: 0,
+				right: [0, null],
 				left: 0,
-				width: 100,
-				height: () => window.innerHeight - 40,
-				margin: '20px',
-				borderRadius: 5,
+				width: ['100%', 100],
+				height: ['54px', '95%'],
+				margin: [null, '20px'],
+				borderRadius: [null, 5],
 				boxShadow:
 					'0px 2px 4px -1px #0003, 0px 4px 5px 0px #00000024, 0px 1px 10px 0px #0000001f',
 			}}
-			value={value}
+			value={value === '' ? 'Dashboard' : value}
 			onChange={handleChange}
 		>
 			{navElements()}
 		</BottomNavigation>
 	);
-
-	const Nav = () => (window.innerWidth < 600 ? <BottomNav /> : <LeftNav />);
-
-	return <Nav />;
 }
--- src/shared/routes/router.tsx
+++ src/shared/routes/router.tsx
@@ -22,7 +22,7 @@ const router = createBrowserRouter([
 		errorElement: <ErrorPage />,
 		children: [
 			{
-				path: '/DriversWorkPlan/Dashboard',
+				path: '/DriversWorkPlan/',
 				element: <Dashboard />,
 			},
 			{
